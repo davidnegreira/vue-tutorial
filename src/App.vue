d5abcec9f723609a4e0053d7839435983b11e467
David Negreira Rios <david.negreirarios.ext@telefonica.com>
2018-08-24 14:02:30 +0200

    normalizado de nombres y estructura

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
     <div id="app">
-        <h1>Tutorial tres con server cambiosiiii!!!</h1>
+        <h1>Tutorial tres con server cambios en vivo, siii!!</h1>
         <button-group/>
     </div>
 </template>
 
 <script>
 import Vue from "vue";
-import ButtonGroup from "./button-group.vue";
+import ButtonGroup from "./components/ButtonGroup.vue";
 export default Vue.extend({
   components: { ButtonGroup }
 });
---
+++ src/components/ButtonGroup.vue
@@ -0,0 +1,30 @@
+<script>
+import Vue from "vue";
+import ButtonCounter from "./ButtonCounter.vue";
+
+export default Vue.extend({
+  components: { ButtonCounter },
+  methods: {
+    mensaje: function() {
+      console.log("pulsar con un cambio");
+    }
+  },
+  data: function() {
+    return {
+      buttons: [{ id: 1, texto: "Boton uno" }, { id: 2, texto: "Boton dos" }]
+    };
+  }
+});
+</script>
+
+<template>
+<div>
+    <ButtonCounter
+      v-on:pulsar="mensaje" 
+      v-for="button in buttons" 
+      v-bind:key="button.id"
+      v-bind:texto="button.texto" />
+</div>
+</template>
+
+
